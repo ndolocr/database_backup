aea190d3a14c75960b16846b88b823ca3ab96459
ndolocr <crndolo@gmail.com>
2024-12-10 13:27:38 +0300

    Returning a response object with a folders content.

--- file_handling/views.py
+++ file_handling/views.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -13,7 +14,15 @@ def desktop_folder_contents(request, folder_name):
     # Verify if the folder exists
     if folder_path.exists() and folder_path.is_dir():
         print(f"Folder Path --> {str(folder_path)}")
-        return str(folder_path)
+        # Get folder contents
+        folder_contents = os.listdir(folder_path)
+        
+        items = {}
+        item_num = 0
+        for item_name in folder_contents:
+            items[item_num] = item_name
+            item_num+=1
+
+        return JsonResponse({"object": items})
     else:
-        print(f"No Desktop Folder found")
-        return None  # Handle case where the folder does not exist
+        return JsonResponse({"object": "Folder Not Found!"})  # Handle case where the folder does not exist
