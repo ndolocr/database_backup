cc5002211607a42078abf0e09d8c42095076a53a
ndolocr <crndolo@gmail.com>
2024-12-10 13:16:55 +0300

    Include Variable folder name

--- file_handling/urls.py
+++ file_handling/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from file_handling import views
 
 urlpatterns = [
-    path("desktop/folder/contents", views.desktop_folder_contents, name="desktop_folder_contents"),
+    path("access/<str:folder_name>", views.desktop_folder_contents, name="desktop_folder_contents"),
 ]
