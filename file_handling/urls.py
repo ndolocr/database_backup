c8929098a48880b21c1301c5b36c351daf9eea65
ndolocr <crndolo@gmail.com>
2024-12-10 15:09:36 +0300

    Rename view and view name

--- file_handling/urls.py
+++ file_handling/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from file_handling import views
 
 urlpatterns = [
-    path("access/<str:folder_name>", views.desktop_folder_contents, name="desktop_folder_contents"),
+    path("access/<str:folder_name>", views.access_folder_contents, name="access_folder_contents"),
 ]
