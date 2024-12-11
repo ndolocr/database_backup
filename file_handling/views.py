0ff0af80008e335f22bffa17a3d8e35036d4fc81
ndolocr <crndolo@gmail.com>
2024-12-11 09:06:10 +0300

    Review view to get folder path

--- file_handling/views.py
+++ file_handling/views.py
@@ -7,16 +7,10 @@ from django.http import JsonResponse
 # Create your views here.
 
 def access_folder_contents(request, folder_name):
-    # Get the user's home directory
-    home_dir = Path.home()    
-    folder_path = home_dir / f"{folder_name}"
+    
+    folder_contents = get_folder_path(folder_name)
 
-    # Verify if the folder exists
-    if folder_path.exists() and folder_path.is_dir():
-        print(f"Folder Path --> {str(folder_path)}")
-        # Get folder contents
-        folder_contents = os.listdir(folder_path)
-        
+    if folder_contents:    
         items = {}
         item_num = 0
         for item_name in folder_contents:
@@ -26,3 +20,18 @@ def access_folder_contents(request, folder_name):
         return JsonResponse({"object": items})
     else:
         return JsonResponse({"object": "Folder Not Found!"})  # Handle case where the folder does not exist
+
+
+def access_subfolders(request, folder_name):
+    pass
+
+
+def get_folder_path(folder_name):
+    # Get the user's home directory
+    home_dir = Path.home()    
+    folder_path = home_dir / f"{folder_name}"
+    
+    if folder_path.exists() and folder_path.is_dir():
+        # folder_contents = os.listdir(folder_path)
+        return os.listdir(folder_path)
+    return None
